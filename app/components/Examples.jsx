67bcbf8f75b1050870f12baf14e9e325745422ac
Rob Pataki <rob@handcrafteddigital.london>
2016-09-07 12:55:11 +0100

    Update title and copy on the About page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,10 @@
 var React = require('react');
 
 var Examples = (props) => {
-  return <h3>Examples</h3>;
+  return (
+    <h3>Examples</h3>
+    <p>Welcome to the Examples page</p>
+  )
 }
 
 module.exports = Examples;
